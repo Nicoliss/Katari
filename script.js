848b8b55e237d172e047a17f92aaf1f3317f12a8
Nicoliss <102914085+Nicoliss@users.noreply.github.com>
2025-02-04 15:05:36 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hola, mundo!");
+const formulario = document.getElementById('formulario-contacto');
+
+formulario.addEventListener('submit', (e) => {
+
+	e.preventDefault();
+ const nombre = document.getElementById('nombre').value.trim();
+ const email = document.getElementById('email').value.trim();
+ const mensaje = document.getElementById('mensaje').value.trim();
+
+ if(nombre && email && mensaje){
+ alert ("Gracias por tu mensaje");
+ }else{
+ alert ("Por favor rellena todos los campos");
+ }
+
+});
